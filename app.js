edccf8335e98155831939ec171c20667053235d3
Your Namemehedimdasif <mehedimdasif@gmail.com>
2019-11-24 03:52:54 +0800

    hosting on heroku

--- app.js
+++ app.js
@@ -1,71 +1,128 @@
+if(process.env.NODE_ENV !== 'production'){
+    require("dotenv").config();
+}
 const express = require('express');
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const bcrypt =require("bcryptjs");
+const flash = require("express-flash");
+const session = require("express-session");
+const override = require("method-override");
 const app = express();
-app.use(bodyParser.json());
-const path = require('path');
+app.use(express.static("public"));
 
-const db = require("./db");
-const collection = "about";
+const passportInitialize = require("./passport-config");
+passportInitialize(passport,
+     email => user_details.find(user => email === user.email),
+      id => user_details.find(user => id === user.id));
 
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'ab.html'));
+app.use(flash());
+app.use(session({
+    secret: process.env.SECRETE_SESSION,
+    save: false,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const user_details = [];
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine","ejs");
+app.use(override("_method"));
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+app.get("/about",(req,res)=>{
+    res.render("about");
+});
+app.get("/blog",(req,res)=>{
+    res.render("blog");
+});
+app.get("/contact",(req,res)=>{
+    res.render("contact");
+});
+app.get("/portfolio-details",(req,res)=>{
+    res.render("portfolio-details");
+});
+app.get("/portfolio",(req,res)=>{
+    res.render("portfolio");
+});
+app.get("/services",(req,res)=>{
+    res.render("services");
+});
+app.get("/single-blog",(req,res)=>{
+    res.render("single-blog");
+});
+app.get("/single",(req,res)=>{
+    res.render("single");
+});
+app.get("/adminblog",(req,res)=>{
+    res.render("adminblog");
 });
 
-app.get('/getAbout',(req,res) => {
-    db.getDB().collection(collection).find({}).toArray((err,documents) => {
-        if(err)
-          console.log(err);
-        else{
-            console.log(documents);
-            res.json(documents);
-        }  
-    });
+
+app.get("/admin",checkNotAuth, (req,res)=>{
+    res.render("admin");
 });
 
-app.put('/:id',(req,res) => {
-
-    const aboutID = req.params.id;
-    const userInput = req.body;
-
-    db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(aboutID)},{$set :{about : userInput.about}},{returnOrginal : false},(err,result) =>{
-        if(err)
-            console.log(err);
-        else
-            res.json(result);    
-    });
-
+app.get("/registration",checkNotAuth,(req,res)=>{
+    res.render("registration");
 });
 
-app.post('/',(req,res)=>{
-    const userInput = req.body;
-    db.getDB().collection(collection).insertOne(userInput,(err,result) =>{
-        if(err)
-           console.log(err);
-        else
-           res.json({result : result, document : result.ops[0]});   
-    });
+app.get("/user",checkAuth,(req, res)=>{
+    res.render("user", {user_details: user_details});
 });
 
-app.delete('/:id',(req,res)=>{
+app.post("/admin",checkNotAuth, passport.authenticate("local",{
+    successRedirect: "/user",
+    failureRedirect: "/admin",
+    failureFlash: true
+}));
 
-    const aboutID = req.params.id;
-    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(aboutID)},(err,result) => {
-        if(err)
-           console.log(err);
-        else
-           res.json(result);   
-    });
-
+app.delete("/logout",(req, res)=>{
+    req.logout();
+    res.redirect('/admin');
 });
 
-db.connect((err) => {
-    if(err){
-        console.log('unable to connect to database');
-        process.exit(1);
-    }
-    else{
-        app.listen(3000,() => {
-            console.log('connected, app listening on port 3000');
+app.post("/registration",checkNotAuth, async (req,res)=>{
+    try {
+        const hashPass = await bcrypt.hash(req.body.password, 10);
+        user_details.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            title: req.body.title,
+            email: req.body.email,
+            password:hashPass
         });
+        res.redirect("/user");
+    } catch (error) {
+        res.redirect("/registration");
     }
-})
+    console.log(user_details);
+    
+});
+
+function checkAuth(req, res, next) {  
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/admin');
+}
+
+function checkNotAuth(req, res, next) {  
+    if(req.isAuthenticated()){
+        return res.redirect('/user');
+    }
+    next();
+}
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
